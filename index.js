ab64ceac3941de851b15e9625583b71479ba5faf
Joshua <joshuawilliams7800@gmail.com>
2022-07-28 07:04:53 -0500

    used if statements inside a switch to organize and execute playRound function

--- index.js
+++ index.js
@@ -8,15 +8,12 @@ let getComputerChoice = () => {
 
     switch (rand) {
         case 0:
-            console.log(rock);
             return rock;
             break;
         case 1:
-            console.log(paper);
             return paper;
             break;
         case 2:
-            console.log(scissors);
             return scissors;
             break;
     }
@@ -27,20 +24,52 @@ let getPlayerChoice = () => {
 
     switch (answer) {
         case "rock":
-            console.log(rock);
+            return rock;
             break;
         case "paper":
-            console.log(paper);
+            return paper;
             break;
         case "scissors":
-            console.log(scissors);
+            return scissors;
             break;
     }
 };
 
-// cpc = computer choice
-let cpc = getComputerChoice();
-// plc = player choice
-let plc = getPlayerChoice();
+let playRound = () => {
+    let cpc = getComputerChoice();
+    let plc = getPlayerChoice();
 
-let playRound = (plc, cpc) => {};
+    console.log(plc);
+    console.log(cpc);
+
+    switch (plc) {
+        case "Rock":
+            if (cpc == "Rock") {
+                console.log("It's a tie. Rocks all.");
+            } else if (cpc == "Paper") {
+                console.log("You Lose. Paper beats Rock.");
+            } else {
+                console.log("You Win. Rock beats Scissors.");
+            }
+            break;
+        case "Paper":
+            if (cpc == "Paper") {
+                console.log("It's a tie. Paper all.");
+            } else if (cpc == "Scissors") {
+                console.log("You Lose. Scissors beats Paper.");
+            } else {
+                console.log("You Win. Paper beats Rock.");
+            }
+
+            break;
+        case "Scissors":
+            if (cpc == "Scissors") {
+                console.log("It's a tie. Rocks all.");
+            } else if (cpc == "Rock") {
+                console.log("You Lose. Rock beats Scissors.");
+            } else {
+                console.log("You Win. Scissors beats Paper.");
+            }
+            break;
+    }
+};
