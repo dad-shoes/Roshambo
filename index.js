f0e98fc0e60625a886656b938e21be56523ecc22
Joshua <joshuawilliams7800@gmail.com>
2022-07-27 22:12:51 -0500

    getPlayerChoice function added and both computer and player choice functions updated with switch intead of if statements

--- index.js
+++ index.js
@@ -1,18 +1,46 @@
 let getRandInt = (max) => Math.floor(Math.random() * max);
+const rock = "Rock";
+const paper = "Paper";
+const scissors = "Scissors";
 
 let getComputerChoice = () => {
-    let rock = "rock";
-    let paper = "Paper";
-    let scissors = "Scissors";
     let rand = getRandInt(3);
-    if (rand === 0) {
-        console.log(rock);
-        return rock;
-    } else if (rand === 1) {
-        console.log(paper);
-        return paper;
-    } else if (rand === 2) {
-        console.log(scissors);
-        return scissors;
+
+    switch (rand) {
+        case 0:
+            console.log(rock);
+            return rock;
+            break;
+        case 1:
+            console.log(paper);
+            return paper;
+            break;
+        case 2:
+            console.log(scissors);
+            return scissors;
+            break;
     }
 };
+
+let getPlayerChoice = () => {
+    let answer = prompt("Rock, Paper, or Scissors?").toLowerCase();
+
+    switch (answer) {
+        case "rock":
+            console.log(rock);
+            break;
+        case "paper":
+            console.log(paper);
+            break;
+        case "scissors":
+            console.log(scissors);
+            break;
+    }
+};
+
+// cpc = computer choice
+let cpc = getComputerChoice();
+// plc = player choice
+let plc = getPlayerChoice();
+
+let playRound = (plc, cpc) => {};
